0adca3ef64b6ff74881ba64436bb0382703b01aa
kyriewei <kyriewei@outlook.com>
2021-04-22 20:43:01 +0800

    ADD BVHNDOE

--- FullRayTracing/FullRayTracing/BVHNode.cpp
+++ FullRayTracing/FullRayTracing/BVHNode.cpp
@@ -1,1 +1,17 @@
 #include "BVHNode.h"
+
+BVHNode::BVHNode(const Scene& scene, float time0, float time1)
+{
+
+}
+
+bool BVHNode::hit(const Ray& r, hit_record& rec, float t_min, float t_max) const
+{
+	if (!box.hit(r, t_min, t_max))
+		return false;
+
+	bool hit_left = left->hit(r, rec, t_min, t_max);
+	bool hit_right = right->hit(r, rec, t_min, hit_left ? rec.t : t_max);
+
+	return hit_left || hit_right;
+}
--- FullRayTracing/FullRayTracing/BVHNode.h
+++ FullRayTracing/FullRayTracing/BVHNode.h
@@ -10,7 +10,10 @@ public:
 	AABB box;
 
 	BVHNode();
-	BVHNode(const Scene& scene, float time0, float time1) : BVHNode()
+	BVHNode(const std::vector<shared_ptr<Object>>& objects, size_t start, size_t end, float time0, float time1);
+	BVHNode(const Scene& scene, float time0, float time1) : BVHNode(scene.objects, 0, scene.objects.size(), time0, time1) {}
 
+	virtual bool hit(const Ray& r, hit_record& rec, float t_min, float t_max) const override;
+	virtual bool bounding_box(float time0, float time1, AABB& output_box) const override;
 };
 
